a9cd4abca4ab38f80f5e8bd3fe4d962ab87e73b6
UematsuYui <yuikoro12@gmail.com>
2018-12-02 15:56:23 +0900

    edit css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,10 @@ import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
 import './all.sass'
+import containerStyles from "./styles/container.module.scss"
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className={containerStyles.container}>
     <Helmet title="YuiTech | webエンジニアの備忘録" />
     <Navbar />
     <div>{children}</div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,41 +3,47 @@ import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
+import navStyles from "./styles/nav.module.scss"
+
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
+  <nav className="is-transparent">
+    <div>
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="aa" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+    </div>
+    <div className={navStyles.navContainer}>
+      <div className="container">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item" to="/products">
+            Products
+          </Link>
+          <Link className="navbar-item" to="/contact">
+            Contact
+          </Link>
+          <Link className="navbar-item" to="/contact/examples">
+            Form Examples
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <a
+            className="navbar-item"
+            href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="icon">
+              <img src={github} alt="Github" />
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   </nav>
